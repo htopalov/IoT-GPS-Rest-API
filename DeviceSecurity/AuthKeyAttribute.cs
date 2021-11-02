100377004f0fa15e14e3907f8f1f8de649de32a3
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-11-02 20:20:07 +0200

    removed method using ado.net and changed back to services dependency injected dbcontext-way less code and more efficient

--- DeviceSecurity/AuthKeyAttribute.cs
+++ DeviceSecurity/AuthKeyAttribute.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Data.SqlClient;
+using Trails.Models.Context;
 using Trails.Models.DTOs;
 using Utilities;
 
@@ -28,11 +27,6 @@ namespace DeviceSecurity
                 return;
             }
 
-            var positionDataFromRequestBody = context.ActionArguments["positionDataDtoPost"] as PositionDataDtoPost;
-            var deviceIdFromRequestBody = positionDataFromRequestBody.DeviceId;
-            var extractedCredentials = await GetCredentialsFromDb(deviceIdFromRequestBody);
-
-
             //check if length of encrypted keys is the same (it has to be)
             //if not doesn't even try to decrypt it
             if (extractedAuthKey.ToString().Length != encryptedKeyLength)
@@ -45,12 +39,22 @@ namespace DeviceSecurity
                 return;
             }
 
+            var positionDataFromRequestBody = context.ActionArguments["positionDataDtoPost"] as PositionDataDtoPost;
+            var deviceIdFromRequestBody = positionDataFromRequestBody.DeviceId;
+
+            var dbContext = context.HttpContext
+                .RequestServices
+                .GetService(typeof(TrailsContext)) as TrailsContext;
+
+            var deviceFromDb = await dbContext.Devices.FindAsync(deviceIdFromRequestBody);
+            var extractedAccessKey = deviceFromDb.AccessKey;
+            var extractedSalt = deviceFromDb.Salt;
+
             var decryptedKey = CryptoGenerator.Decrypt(extractedAuthKey);
-            var extractedSalt = extractedCredentials["Salt"];
-            var restoredAccessKey = HashGenerator.ComputeHash(Encoding.UTF8.GetBytes(decryptedKey), Encoding.UTF8.GetBytes(extractedSalt));
+            var restoredAccessKey = HashGenerator.ComputeHash(Encoding.UTF8.GetBytes(decryptedKey),
+                Encoding.UTF8.GetBytes(extractedSalt));
 
-
-            if (extractedCredentials["AccessKey"] != restoredAccessKey)
+            if (extractedAccessKey != restoredAccessKey)
             {
                 context.Result = new ContentResult()
                 {
@@ -61,32 +65,6 @@ namespace DeviceSecurity
             }
 
             await next();
-        }
-
-
-
-        //using ado. net to retrieve hashed key and salt for specific device and return it
-        private async Task<Dictionary<string,string>> GetCredentialsFromDb(string deviceIdFromRequestBody)
-        {
-            var credentials = new Dictionary<string, string>();
-
-            await using (var connection = new SqlConnection(Config.CONNECTION_STRING))
-            {
-                await connection.OpenAsync();
-                var command = new SqlCommand(Config.sqlCommandStr, connection);
-                command.Parameters.AddWithValue("@deviceId", deviceIdFromRequestBody);
-                var sqlReader = await command.ExecuteReaderAsync();
-                await using (sqlReader)
-                {
-                    while (await sqlReader.ReadAsync())
-                    {
-                        credentials.Add("AccessKey", sqlReader.GetString(0));
-                        credentials.Add("Salt", sqlReader.GetString(1));
-                    }
-                }
-            }
-
-            return credentials;
         }
     }
 }
--- Utilities/Config.cs
+++ Utilities/Config.cs
@@ -4,7 +4,6 @@
     {
         //TODO: Change this values to secrets!
         public const string CONNECTION_STRING = "Server=.;Integrated Security = true; Database=TrailsDB";
-        public const string sqlCommandStr = "SELECT AccessKey,Salt FROM Devices WHERE DeviceId = @deviceId";
         public const string EncryptionKey = "thisisyourcode!!";
  
     }
